1c088978dc2eb68035d71b3b35e87a2780b1e082
Adam Kanstrup Hviid <Hviidadam@gmail.com>
2026-03-29 13:37:55 +0200

    vite create react app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1>Adam + Amy</h1>
+      <h2>Save the Date</h2>
+      <p>27 marts 2027</p>
+
+      <h3>The Grand Hotel</h3>
+      <h4>Taipei, Taiwan</h4>
+    </div>
+  );
+}
+
+export default App;
